a97ac7961b61ff156306e66ab1659cf2804934fe
lozinskaya <lozinskaya21@rambler.ru>
2021-12-17 16:14:34 +0500

    upd

--- src/app/Pages/Event/EventPage.tsx
+++ src/app/Pages/Event/EventPage.tsx
@@ -1,22 +1,9 @@
-import React, {useEffect, useState}  from 'react';
+import React from 'react';
 import {makeStyles, Modal, TableCell, TableHead, Typography, IconButton, Table, TableBody, TableRow} from "@material-ui/core";
-import Paper from '@mui/material/Paper';
-import TableContainer from '@mui/material/TableContainer';
-import CustomButton from "../../components/CustomButton";
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import DownloadIcon from '@mui/icons-material/Download';
-import CopyAllIcon from '@mui/icons-material/CopyAll';
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-import {useSearchData} from "./effects/use-search-data.effect";
-import {Event} from "../../../utils/interface";
-import dateFormat from 'dateformat';
+import CustomButton from '../../components/CustomButton';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
+import EditIcon from '@material-ui/icons/Edit';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import AddEvent from "./AddEvent";
 import {useEventData} from "./effects/use-event-data.effect";
@@ -55,11 +42,9 @@ const useStyles = makeStyles(theme => ({
 const columns = [
     { id: 'title', label: 'Название', minWidth: 170 },
     { id: 'date', label: 'Дата', minWidth: 100 },
-    { id: 'over_score ', label: 'Оценка', minWidth: 100 },
-    { id: 'scores_count ', label: 'Количество оценок', minWidth: 100 },
+    { id: 'score ', label: 'Оценка', minWidth: 100 },
     { id: 'btns', label: '', minWidth: 10 },
 ];
-  
 
 export const EventPage: React.FC<EventPageProps> = (props: EventPageProps) => {
     const classes = useStyles()
@@ -79,7 +64,6 @@ export const EventPage: React.FC<EventPageProps> = (props: EventPageProps) => {
         navigator.clipboard.writeText(BASE_URL + `/rate/${id}`)
     }
 
-    console.log(data)
     return (
         <div className={classes.root}>
             <div className={classes.btn}>
